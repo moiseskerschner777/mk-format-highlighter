637f98b81aaa7dc4304a62c479bc82c9397945d4
M. Kerschner <moises.kerschner@shift.com.br>
2026-06-17 02:08:19 -0300

    add 33333.svg and yaml.svg icons, update mk.svg, and integrate 33333.svg in MkFileType

--- src/main/kotlin/com/moiseskerschner/mkformat/MkFileType.kt
+++ src/main/kotlin/com/moiseskerschner/mkformat/MkFileType.kt
@@ -5,9 +5,10 @@ import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
 object MkFileType : LanguageFileType(MkLanguage) {
+    private val ICON: Icon = IconLoader.getIcon("/icons/mk.svg", MkFileType::class.java)
+
     override fun getName(): String = "mk-format"
     override fun getDescription(): String = "mk-format outline file"
     override fun getDefaultExtension(): String = "mk"
-    private val ICON: Icon = IconLoader.getIcon("/icons/mk.svg", MkFileType::class.java)
     override fun getIcon(): Icon = ICON
 }
